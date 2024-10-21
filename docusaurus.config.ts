390918b8d8ff23aa5f6172f527559fc6f2789e70
jafayer <github@fayer.me>
2024-10-21 16:31:36 -0400

    Update config and add automated build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jafayer', // Usually your GitHub org/user name.
-  projectName: 'DinoDNS', // Usually your repo name.
+  projectName: 'dinodns-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
